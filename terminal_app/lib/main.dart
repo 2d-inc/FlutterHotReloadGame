d676a1e83211645330c93c7a5296fc2dcc7a7d0b
Umberto Sonnino <rimher@gmail.com>
2018-04-03 17:04:45 -0700

    Start page design

--- terminal_app/lib/main.dart
+++ terminal_app/lib/main.dart
@@ -6,6 +6,8 @@ import "package:web_socket_channel/io.dart";
 import "dart:io";
 void main() => runApp(new MyApp());
 
+enum PlayerStatus { READY, NOT_READY }
+
 class MyApp extends StatelessWidget {
 	// This widget is the root of your application.
 	@override
@@ -43,43 +45,158 @@ class MyHomePage extends StatefulWidget
 
 class _MyHomePageState extends State<MyHomePage> 
 {
+	static const Color START_BUTTON_FADED = const Color.fromARGB(204, 9, 45, 51);
+	static const Color START_BUTTON_LIT = const Color.fromARGB(255, 86, 234, 246);
+	
+	List<Widget> _buttonList;
+	final List<PlayerStatus> _players = new List.filled(4, PlayerStatus.NOT_READY);
+	PlayerStatus _status;
+	Container _readyButton;
+	Container _startButton;
+
+
 	void _handleTap()
 	{
 		widget.socket.add("hi");
 	}
 
+	void _handleReady()
+	{
+		// TODO:
+	}
+
+	void _handleStart()
+	{
+		// TODO:
+	}
+
 	@override
 	Widget build(BuildContext context) 
 	{
-		//new GestureDetector(onTap: _handleTap, child:
+		final double DPR = MediaQuery.of(context).devicePixelRatio;
+
 		return new Container(
 			decoration:new BoxDecoration(color:Colors.white),
 			child:new Row(
 				children: <Widget>[
 					new Container(
-						width:500.0
+						width: MediaQuery.of(context).size.width * 0.66
 					),
 					new Expanded(
 						child:new Container(
-							padding:const EdgeInsets.fromLTRB(50.0, 50.0, 50.0, 50.0),
+							padding: new EdgeInsets.all(12.0 * DPR),
 							decoration:new BoxDecoration(color:Colors.black),
-							child:new Column(
-								crossAxisAlignment: CrossAxisAlignment.start,
-								children: <Widget>[
-									new Container(
-										padding:const EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 50.0),
-										child:new Text("Terminal App", style:const TextStyle(fontSize:12.0,color:Colors.white, decoration: TextDecoration.none))
-									),
-									new Expanded(
-										child:new ControlGrid(
-											children:<Widget>[
-												new GestureDetector(onTap: _handleTap, child:new Container(decoration:new BoxDecoration(color:Colors.red))),
-												new GestureDetector(onTap: _handleTap, child:new Container(decoration:new BoxDecoration(color:Colors.green))),
-												new GestureDetector(onTap: _handleTap, child:new Container(decoration:new BoxDecoration(color:Colors.blue))),
+							child: new Container(
+								decoration: new BoxDecoration(border: new Border.all(color: const Color.fromARGB(127, 72, 196, 206)), borderRadius: new BorderRadius.circular(3.0)),
+								padding: new EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 6.0),
+								child: new Column(
+									crossAxisAlignment: CrossAxisAlignment.start,
+									children: 
+									[
+										// Title Row
+										new Row(children: 
+											[	
+												new Text("SYSTEM ONLINE", style: new TextStyle(color: new Color.fromARGB(255, 167, 230, 237), fontFamily: "Inconsolata", fontSize: 6.0, decoration: TextDecoration.none, letterSpacing: 0.4)),
+												new Text(" > MILESTONE INITIATED", style: new TextStyle(color: new Color.fromARGB(255, 86, 234, 246), fontFamily: "Inconsolata", fontSize: 6.0, decoration: TextDecoration.none, letterSpacing: 0.5))
 											]
-										)
-									)
-								]
+										),
+										new Row(children: [ new Expanded(child: new Container(margin: new EdgeInsets.only(top:5.0), color: const Color.fromARGB(77, 167, 230, 237), height: 1.0)) ]),
+										new Row(children: [ new Expanded(child: new Container(margin: new EdgeInsets.only(top:5.0), color: const Color.fromARGB(77, 167, 230, 237), height: 1.0)) ]), 
+										// Players Row
+										new Column(
+											children: 
+											[
+												new Container(
+													margin: new EdgeInsets.only(top: 24.0),
+													decoration: new BoxDecoration(border: new Border.all(width: 2.0 , color: const Color.fromARGB(255, 62, 196, 206)), borderRadius: new BorderRadius.circular(3.0), color: const Color.fromARGB(255, 3, 28, 32)),
+													padding: new EdgeInsets.symmetric(vertical: 14.0, horizontal: 18.0),
+													child: new Column(
+														children: 
+														[
+															// TODO: make this into its own component
+															new Row(
+																crossAxisAlignment: CrossAxisAlignment.end,
+																children:
+																[ 
+																	new Text("Player 1", style: new TextStyle(color: new Color.fromARGB(255, 45, 207, 220), fontFamily: "Inconsolata", fontWeight: FontWeight.w100, fontSize: 18.0, decoration: TextDecoration.none)),
+																	new Expanded(child: new Container(color: new Color.fromARGB(255, 43, 196, 209), height: 1.0, margin: new EdgeInsets.only(left: 10.0, right: 10.0, bottom: 4.0))),
+																	new Text("READY", style: new TextStyle(color: new Color.fromARGB(255, 86, 234, 236), fontFamily: "Inconsolata", fontWeight: FontWeight.w700, fontSize: 18.0, decoration: TextDecoration.none)),
+																]
+															),
+															new Row(
+																crossAxisAlignment: CrossAxisAlignment.end,
+																children:
+																[ 
+																	new Text("Player 2", style: new TextStyle(color: new Color.fromARGB(255, 45, 207, 220), fontFamily: "Inconsolata", fontWeight: FontWeight.w100, fontSize: 18.0, decoration: TextDecoration.none)),
+																	new Expanded(child: new Container(color: new Color.fromARGB(255, 43, 196, 209), height: 1.0, margin: new EdgeInsets.only(left: 10.0, right: 10.0, bottom: 4.0))),
+																	new Text("READY", style: new TextStyle(color: new Color.fromARGB(255, 86, 234, 236), fontFamily: "Inconsolata", fontWeight: FontWeight.w700, fontSize: 18.0, decoration: TextDecoration.none)),
+																]
+															),
+															new Row(
+																crossAxisAlignment: CrossAxisAlignment.end,
+																children:
+																[ 
+																	new Text("Player 3", style: new TextStyle(color: new Color.fromARGB(255, 45, 207, 220), fontFamily: "Inconsolata", fontWeight: FontWeight.w100, fontSize: 18.0, decoration: TextDecoration.none)),
+																	new Expanded(child: new Container(color: new Color.fromARGB(255, 43, 196, 209), height: 1.0, margin: new EdgeInsets.only(left: 10.0, right: 10.0, bottom: 4.0))),
+																	new Text("NOT READY", style: new TextStyle(color: new Color.fromARGB(255, 22, 75, 81), fontFamily: "Inconsolata", fontWeight: FontWeight.w100, fontSize: 18.0, decoration: TextDecoration.none)),
+																]
+															),
+															new Row(
+																crossAxisAlignment: CrossAxisAlignment.end,
+																children:
+																[ 
+																	new Text("Player 4", style: new TextStyle(color: new Color.fromARGB(255, 45, 207, 220), fontFamily: "Inconsolata", fontWeight: FontWeight.w100, fontSize: 18.0, decoration: TextDecoration.none)),
+																	new Expanded(child: new Container(color: new Color.fromARGB(255, 43, 196, 209), height: 1.0, margin: new EdgeInsets.only(left: 10.0, right: 10.0, bottom: 4.0))),
+																	new Text("NOT READY", style: new TextStyle(color: new Color.fromARGB(255, 22, 75, 81), fontFamily: "Inconsolata", fontWeight: FontWeight.w100, fontSize: 18.0, decoration: TextDecoration.none)),
+																]
+															),
+														],
+													)
+												),
+											]
+										),
+										new Container(
+												margin: new EdgeInsets.only(top: 5.0),
+												child: new Row(
+													children:
+													[
+														new Text("COMMAND", style: new TextStyle(color: new Color.fromARGB(255, 167, 230, 237), fontFamily: "Inconsolata", fontWeight: FontWeight.bold, fontSize: 6.0, decoration: TextDecoration.none, letterSpacing: 0.4)),
+														new Text(" PANEL", style: new TextStyle(color: new Color.fromARGB(255, 86, 234, 246), fontFamily: "Inconsolata", fontWeight: FontWeight.bold, fontSize: 6.0, decoration: TextDecoration.none, letterSpacing: 0.5))
+													],
+												)
+											),
+											// Fill the middle space
+											new Expanded(child: new Container()),
+											// Buttons TODO: make this into its own component
+											new Column(
+												children:
+												[
+													new GestureDetector(
+														onTap: _handleReady,
+														child: _readyButton
+													),
+													new GestureDetector(
+														onTap: _handleStart,
+														child: new Container(
+															margin: const EdgeInsets.only(top: 10.0),
+															decoration: new BoxDecoration(borderRadius: new BorderRadius.circular(3.0), color: const Color.fromARGB(204, 9, 45, 51)),
+															child: new Container(
+																height: 59.0,
+																alignment: Alignment.center,
+																child: new Text("START", style: const TextStyle(color: const Color.fromARGB(51, 167, 230, 237), fontFamily: "Inconsolata", fontWeight: FontWeight.bold, fontSize: 18.0, decoration: TextDecoration.none, letterSpacing: 1.3))
+															)
+														)
+													)
+												],
+											),
+											new Container(
+												margin: new EdgeInsets.only(top: 10.0),
+												alignment: Alignment.bottomRight,
+												child: new Text("V0.1", style: const TextStyle(color: const Color.fromARGB(255, 50, 69, 71), fontFamily: "Inconsolata", fontWeight: FontWeight.bold, fontSize: 12.0, decoration: TextDecoration.none, letterSpacing: 0.9))
+											),
+											new Row(children: [ new Expanded(child: new Container(margin: new EdgeInsets.only(top:5.0), color: const Color.fromARGB(77, 167, 230, 237), height: 1.0)) ]),
+									]
+								)
 							)
 						)
 					)
