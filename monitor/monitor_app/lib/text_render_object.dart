e77ae8a6c0d72f99e005e12114e756be0e318d5e
Umberto Sonnino <rimher@gmail.com>
2018-04-17 18:35:59 -0700

    Restyling for the code_box

--- monitor/monitor_app/lib/text_render_object.dart
+++ monitor/monitor_app/lib/text_render_object.dart
@@ -36,9 +36,15 @@ class Highlight
 
 class TextRenderObject extends RenderBox
 {
-	static const double FONT_SIZE = 17.0;
-	static const String FONT_FAMILY = "RobotoMono";
+	static const double FONT_SIZE = 16.0;
+	static const String FONT_FAMILY = "Inconsolata";
+	static const FontWeight FONT_WEIGHT = FontWeight.w100;
+	static const double LINE_HEIGHT_MULTIPLIER = 18.0/16.0;
 	static const double LINES_NUM_WIDTH = 60.0;
+	static const int LINES_RECT_PADDING = 15;
+	static const int CODE_PADDING_LEFT = 15;
+	static const int CODE_PADDING_TOP = 10;
+	static const int HIGHLIGHT_PADDING = 1;
 
 	String _text;
 	TextStyle style;
@@ -46,27 +52,33 @@ class TextRenderObject extends RenderBox
 	int _highlightAlpha;
 	int _maxLines;
 	double _lineScrollOffset;
-	double _lineHeight;
+	double _glyphHeight = 10.0;
+	double _glyphWidth = 10.0;
 
 	ui.Paragraph _codeParagraph;
 	ui.Paragraph _linesParagraph;
 
 	TextRenderObject() : 
 		this._lineScrollOffset = 0.0,
-		this._highlight = new Highlight(0, 0, 0),
-		this._highlightAlpha = 100
+		this._highlight = new Highlight(-1, 0, 0),
+		this._highlightAlpha = 56
 	{
 		// Initialize the Line Height for this style
 		ui.ParagraphStyle codeStyle = new ui.ParagraphStyle(
 				fontFamily: FONT_FAMILY,
-				fontSize: FONT_SIZE
+				fontSize: FONT_SIZE,
+				lineHeight: LINE_HEIGHT_MULTIPLIER,
+				// fontWeight: FONT_WEIGHT
 			);
 
 		ui.ParagraphBuilder pb = new ui.ParagraphBuilder(codeStyle);
 		
+		String numText = "0";
+		pb.addText(numText);
 		ui.Paragraph singleLine = pb.build()..layout(new ui.ParagraphConstraints(width: double.maxFinite));
-		this._lineHeight = singleLine.height;
-		pb.addText("Loading...");
+		List<ui.TextBox> boxes = singleLine.getBoxesForRange(0, numText.length);
+		this._glyphHeight = boxes.last.bottom - boxes.first.top;
+		this._glyphWidth = boxes.last.right - boxes.first.left;
 	}
 
 	@override
@@ -85,10 +97,12 @@ class TextRenderObject extends RenderBox
 	performLayout()
 	{
 		super.performLayout();
-			
+
 		ui.ParagraphStyle codeStyle = new ui.ParagraphStyle(
 					fontFamily: FONT_FAMILY,
-					fontSize: FONT_SIZE
+					fontSize: FONT_SIZE,
+					lineHeight: LINE_HEIGHT_MULTIPLIER,
+					// fontWeight: FONT_WEIGHT
 				);
 
 		ui.ParagraphBuilder codePB = new ui.ParagraphBuilder(codeStyle);
@@ -96,70 +110,86 @@ class TextRenderObject extends RenderBox
 				new ui.ParagraphStyle(
 					textAlign: TextAlign.right,
 					fontFamily: FONT_FAMILY,
-					fontSize: FONT_SIZE
+					fontSize: FONT_SIZE,
+					lineHeight: LINE_HEIGHT_MULTIPLIER,
+					// fontWeight: FONT_WEIGHT,
 				)
 			);
+		ui.TextStyle semiTransparent = new ui.TextStyle(color: new Color.fromRGBO(255, 255, 255, 0.5));
+		ui.TextStyle opaque = new ui.TextStyle(color: Colors.white);
 
+		codePB.pushStyle(semiTransparent);
+		linesPB.pushStyle(semiTransparent);
 		ui.ParagraphConstraints codeConstraints = new ui.ParagraphConstraints(width: double.maxFinite);
-		ui.ParagraphConstraints lineConstraints = new ui.ParagraphConstraints(width: 50.0);
+
+		int maxNumDigits = _maxLines.toString().length;
+		ui.ParagraphConstraints lineConstraints = new ui.ParagraphConstraints(width: maxNumDigits*_glyphWidth);
 		
 		String actualText = _text ?? "Loading...";
 		List<String> lines = actualText.split('\n');
 
-		int currentLineNum = this.topLineNumber.clamp(0, lines.length - 1);
-		double maxHeight = size.height;
-		double currentHeight = 0.0;
+		int topLine = this.topLineNumber.clamp(0, lines.length - 1);
 
-		String visibleText = lines[currentLineNum].replaceAll('\t', "  ");
-		String visibleLineNums = currentLineNum.toString();
+		double paragraphLineHeight = _glyphHeight;
+		int maxVisibleLines = (size.height/paragraphLineHeight).ceil() + 2;
+		List<String> visibleLines = lines.sublist(topLine, (topLine + maxVisibleLines).clamp(0, lines.length -1));
 
-		for(int i = currentLineNum + 1;
-			i < lines.length && currentHeight < maxHeight; 
-			++i, ++currentLineNum)
+		int highlightStart = _highlight.row;
+		int highlightEnd = _highlight.row + _highlight.howManyLines;
+
+		for(int i = 0; i < visibleLines.length; i++)
 		{
-			visibleText += "\n" + lines[i].replaceAll('\t', "  ");
-			visibleLineNums += '\n$i';
-			
-			ui.ParagraphBuilder tempPB = new ui.ParagraphBuilder(codeStyle);
-			
-			tempPB.addText(visibleText);
-			ui.Paragraph _tempParagraph = tempPB.build()
-				..layout(codeConstraints);
+			String l = visibleLines[i].replaceAll('\t', "  ");
+			ui.TextStyle lineStyle;
+			int currentLine = topLine + i;
+			if(currentLine >= highlightStart && currentLine < highlightEnd)
+			{
+				lineStyle = opaque;
+			}
+			else
+			{
+				lineStyle = semiTransparent;
+			}
+			codePB.pushStyle(lineStyle);
+			codePB.addText("$l\n");
+			codePB.pop();
 
-			// Adjust the scrolling to be continuous rather than discrete
-			currentHeight = _tempParagraph.height - (_lineScrollOffset % _lineHeight);
+			linesPB.pushStyle(lineStyle);
+			linesPB.addText("${i+topLine}\n");
+			linesPB.pop();
 		}
 
-		codePB.addText(visibleText);
 		_codeParagraph = codePB.build()..layout(codeConstraints);
-		linesPB.addText(visibleLineNums);
 		_linesParagraph = linesPB.build()..layout(lineConstraints);
 	}
 
 	@override
 	void paint(PaintingContext context, Offset offset)
 	{
+		offset = offset.translate(0.0, -2.0); // FIXME: fix  the node alignment instead of translating
+
 		final Canvas canvas = context.canvas;
 		canvas.save();
 		canvas.clipRect(offset&size);
-		// TODO: debug only
-		// Size codeBoxSize = new Size(size.width - LINES_NUM_WIDTH, size.height);
-		// Offset codeBoxOffset = new Offset(offset.dx + LINES_NUM_WIDTH, offset.dy);
-		// canvas.drawRect(codeBoxOffset&size, new Paint()..color = new Color.fromARGB(200, 70, 70, 70));
-		//
-		Size lineRectSize = new Size(LINES_NUM_WIDTH, size.height);
-		canvas.drawRect(offset&lineRectSize, new Paint()..color = new Color.fromARGB(200, 59, 60, 61));
 
-		double scrollAdjustment = (_lineScrollOffset % _lineHeight);
-		double highlightYOffset = (_highlight.row - this.topLineNumber).toDouble();
-		Size highlightSize = new Size(size.width - LINES_NUM_WIDTH, _lineHeight * _highlight.howManyLines);
-		Offset highlightOffset = new Offset(offset.dx + LINES_NUM_WIDTH, offset.dy + _lineHeight*highlightYOffset - scrollAdjustment);
-		RRect rounded = new RRect.fromRectXY(highlightOffset&highlightSize, 5.0, 5.0);
-		canvas.drawRRect(rounded, new Paint()..color = new Color.fromARGB(_highlightAlpha, 212, 225, 87));
+		int maxNumDigits = _maxLines.toString().length;
+		Size linesRectSize = new Size(LINES_RECT_PADDING*2 + _glyphWidth*maxNumDigits, size.height);
+		canvas.drawRect(offset&linesRectSize, new Paint()..color = new Color.fromARGB(51, 0, 0, 0));
 
-		canvas.drawParagraph(_linesParagraph, new Offset(offset.dx, offset.dy - scrollAdjustment ));
-		const int CODE_PADDING = 10;
-		canvas.drawParagraph(_codeParagraph, new Offset(offset.dx + LINES_NUM_WIDTH + CODE_PADDING, offset.dy - scrollAdjustment));
+		double codeBoxTop = offset.dy + CODE_PADDING_TOP;
+
+		if(_highlight.howManyLines > 0)
+		{
+			Size highlightSize = new Size(size.width, 22.0);
+			int highlightLineNumber = _highlight.row - this.topLineNumber;
+			Offset highlightOffset = new Offset(
+				offset.dx,
+				codeBoxTop + _glyphHeight * highlightLineNumber + HIGHLIGHT_PADDING
+			);
+			canvas.drawRect(highlightOffset&highlightSize, new Paint()..color = new Color.fromARGB(_highlightAlpha, 255, 0, 108));
+		}
+		canvas.drawParagraph(_linesParagraph, new Offset(offset.dx + LINES_RECT_PADDING, codeBoxTop));
+		canvas.drawParagraph(_codeParagraph, new Offset(offset.dx + linesRectSize.width + CODE_PADDING_LEFT, codeBoxTop));
 		canvas.restore();
 	}
 
@@ -169,7 +199,7 @@ class TextRenderObject extends RenderBox
 		{
 			this._highlight = h;
 			// Try to keep the highlight always in the center of the scroll area
-			// this.scrollValue = (h.row-10) * _lineHeight;
+			// this.scrollValue = (h.row-10) * _glyphHeight;
 			
 			markNeedsLayout();
 			markNeedsPaint();
@@ -182,13 +212,13 @@ class TextRenderObject extends RenderBox
 
 		if(this._lineScrollOffset != lineNumber)
 		{
-			double max = (_maxLines-1) * _lineHeight;
+			double max = (_maxLines-1) * _glyphHeight;
 
 			// calcualte offset by line number and center of screen.
-			double offset = lineNumber * _lineHeight;
+			double offset = lineNumber * _glyphHeight;
 
 			offset -= size.height/2.0; // go down to center of screen
-			offset += _lineHeight/2.0; // go back up by half of the line
+			offset += _glyphHeight/2.0; // go back up by half of the line
 
 			this._lineScrollOffset = min(max, offset);
 
@@ -201,7 +231,7 @@ class TextRenderObject extends RenderBox
 	{
 		if(value == null)
 		{
-			print("TRYING TO SET THE TEXT TO A NULL VALUE");
+			// print("TRYING TO SET THE TEXT TO A NULL VALUE");
 			return;
 		}
 		else if(this._text != value)
@@ -213,7 +243,7 @@ class TextRenderObject extends RenderBox
 		}
 	}
 	
-	get topLineNumber => (_lineScrollOffset / _lineHeight).floor();
+	get topLineNumber => (_lineScrollOffset / _glyphHeight).floor();
 
 	set highlightAlpha(int value)
 	{
